b6bdeaec5e87d011bc1d428c90be87e379de7dd2
Julio Montaño <jmzvetica@gmail.com>
2012-11-17 21:17:40 -0600

    Agregue layouts para acomodar todo, esta comentado

--- ArcViewer.java
+++ ArcViewer.java
@@ -6,7 +6,8 @@ import java.awt.*;
 
 public class ArcViewer extends JPanel {
 	
-	private JPanel ventana;
+	private JPanel areaventana;
+	private JPanel areabotones;
 	private JLabel imagen;
 
 	private JButton presentacion;
@@ -20,9 +21,12 @@ public class ArcViewer extends JPanel {
 
 	private JMenuBar menu;
 
+	Color colorGris = new Color(0x999999);
+
 	public void init() {
 		
-		ventana = new JPanel();
+		areaventana = new JPanel();
+		areabotones = new JPanel();
 		imagen = new JLabel("Aca va la imagen");
 
 		presentacion = new JButton("play");
@@ -33,13 +37,21 @@ public class ArcViewer extends JPanel {
 
 		carpeta = new JButton("Seleccionar Carpeta");
 
-		ventana.add(imagen);
-		ventana.add(presentacion);
-		ventana.add(siguente);
-		ventana.add(atras);
-		ventana.add(zoom);
-		ventana.add(carpeta);
+		/* Agregando los componentes */
 
+		atras.setBackground(colorGris);
+
+		add(imagen);
+		areabotones.add(presentacion);
+		areabotones.add(siguente);
+		areabotones.add(atras);
+		areabotones.add(zoom);
+		areabotones.add(carpeta);
+
+
+		this.setLayout (new BorderLayout()); //layout para todo el programa
+		add(areaventana, BorderLayout.CENTER); //layout solo para el area de muestra de imagen
+		add(areabotones, BorderLayout.SOUTH); //layout para todos los botones
 	}
 
 	public ArcViewer() {
@@ -47,13 +59,15 @@ public class ArcViewer extends JPanel {
 		init(); 
 		//start();
 
-		JFrame frame = new JFrame("hola");
+		JFrame frame = new JFrame("ArcViewer v.0.1");
+			
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Cierra el programa por la buena
+
 		frame.add("Center", this);
 		frame.setSize(900, 600);
 		frame.setVisible(true); }
 
+
 	public static void main(String[] args) {
-		
-		new ArcViewer();
-	}
+		new ArcViewer(); }
 }
